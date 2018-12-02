be5fcf34b9213df6e603dc4a6677964c103b1c87
David Bulczak (Win) <david.bulczak@gmail.com>
2018-12-02 21:44:49 +0100

    Day 01 - Part 01 done quickly. Cleanup incoming :P

--- adventofcode2018/Program.cs
+++ adventofcode2018/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace adventofcode2018
 {
-    class Program
+    class Day01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string appPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(System.AppContext.BaseDirectory, "..//..//.."));
+            System.Console.WriteLine(appPath);
+            
+            // Process command line input
+            if (args.Length > 0)
+            {
+                string inputPart01FilePath = System.IO.Path.Combine(appPath, args[0]);
+                System.Console.WriteLine(inputPart01FilePath);
+                string[] frequencyStrings = System.IO.File.ReadAllLines(inputPart01FilePath);
+                int[] frequencies = Array.ConvertAll(frequencyStrings, int.Parse);
+                // sum up frequencies (non LINQ version)
+                int result = 0;
+                Array.ForEach(frequencies, delegate (int i) { result += i; });
+                System.Console.WriteLine("Day 01 - Part 01");
+                System.Console.WriteLine("Bzzzt... The final frequency resulting from received input is " + result);
+
+            }
+            else
+            {
+                System.Console.WriteLine("Bzzzzt... ERROR: No input received.");
+            }
         }
     }
 }
