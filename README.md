4fd0ed94d82cf9f844070b80f893d78dbd28a700
David Bulczak (Win) <david.bulczak@gmail.com>
2018-12-02 23:16:22 +0100

    Some cleanup and refactoring.

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # adventofcode2018
+
+I want to take this years (2018) advent of code as a chance to learn C#. This means that I priorize using C#'s features sets over elegant or compact solutions. Still every kind of feedback and hints for improvement are welcome. 
+
+Thus in short here are my goals:
+### Major goals
+- Learn C#
+    - Syntax
+    - Core features
+    - Advanced features
+    - Library
+    - Best practices
+- Having fun
+
+### Minor goals 
+- Elegant solutions
+- Use/develop fancy algorithms to solve problems
+
+## Diary
+
+### Day 01
+I've used the tasks of this day to get used to C# in Visual Studio and to learn some of it's syntactic basics, which includes
+
+- General application structure
+    - Main function
+    - using ...
+    - namespace class Main base structure
+- System.Console commands
+- System.IO commands for file reading
+- Declaration and definition of functions
+- Array.ForEach to process whole array
+- List<> to track values and get used to C# generics
--- adventofcode2018/Program.cs
+++ adventofcode2018/Program.cs
@@ -11,51 +11,60 @@ namespace adventofcode2018
             return Array.ConvertAll(tmpFrequencyString, int.Parse);
         }
 
+        private static string GetAppPath()
+        {
+            return System.IO.Path.GetFullPath(System.IO.Path.Combine(System.AppContext.BaseDirectory, "..//..//.."));
+        }
+
+        private static void Part01()
+        {
+            System.Console.WriteLine("----------------");
+            System.Console.WriteLine("Day 01 - Part 01");
+            System.Console.WriteLine("----------------");
+
+            int[] frequencies       = ReadFrequencies(System.IO.Path.Combine(GetAppPath(), "input-part-02.txt")); 
+            
+            // sum up frequencies (non LINQ version)
+            int sum = 0;
+            Array.ForEach(frequencies, delegate (int i) { sum += i; });
+            System.Console.WriteLine("Bzzzt... The final frequency resulting from received input is " + sum);
+        }
+
+        private static void Part02()
+        {
+            System.Console.WriteLine("----------------");
+            System.Console.WriteLine("Day 01 - Part 02");
+            System.Console.WriteLine("----------------");
+
+            int[] frequencies = ReadFrequencies(System.IO.Path.Combine(GetAppPath(), "input-part-02.txt"));
+            
+            List<int> usedFrequencies = new List<int>() { 0 }; /// tracks used frequencies
+            int sum = 0;
+            bool done = false;
+            int firstDuplicate = 0;
+            while (!done)
+            {
+                for (int i = 0; i < frequencies.Length && !done; ++i)
+                {
+                    sum += frequencies[i];
+                    if (!usedFrequencies.Contains(sum))
+                        usedFrequencies.Add(sum);
+                    else
+                    {
+                        done = true;
+                        firstDuplicate = sum;
+                    }
+                }
+            }
+            System.Console.WriteLine("Bzzzzt... frequency " + firstDuplicate + " has been reached twice.");
+        }
+
         static void Main(string[] args)
         {
             string appPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(System.AppContext.BaseDirectory, "..//..//.."));
-            System.Console.WriteLine(appPath);
-            
-            // Process command line input
-            if (args.Length > 0)
-            {
-                string inputPart01FilePath = System.IO.Path.Combine(appPath, args[0]);
-                int[] frequenciesPart01 = ReadFrequencies(inputPart01FilePath);
-                
-                // sum up frequencies (non LINQ version)
-                int result = 0;
-                Array.ForEach(frequenciesPart01, delegate (int i) { result += i; });
-                System.Console.WriteLine("Day 01 - Part 01");
-                System.Console.WriteLine("Bzzzt... The final frequency resulting from received input is " + result);
 
-                int[] frequenciesPart02 = ReadFrequencies(System.IO.Path.Combine(appPath, "input-part-02.txt"));
-                List<int> usedFrequencies = new List<int>();
-                int sumPart02 = 0;
-                bool done = false;
-                int firstDuplicate = 0;
-                usedFrequencies.Add(0); // initial frequency has to be added since it also can be visited multiple times
-                //foreach (int freq in frequenciesPart02)
-                while (!done)
-                {
-                    for (int i = 0; i < frequenciesPart02.Length && !done; ++i)
-                    {
-                        sumPart02 += frequenciesPart02[i];
-                        if (!usedFrequencies.Contains(sumPart02))
-                            usedFrequencies.Add(sumPart02);
-                        else
-                        {
-                            done = true;
-                            firstDuplicate = sumPart02;
-                        }
-                    }
-                }
-                System.Console.WriteLine("Day 01 - Part 02");
-                System.Console.WriteLine("Bzzzzt... frequency " + firstDuplicate + " has been reached twice.");
-            }
-            else
-            {
-                System.Console.WriteLine("Bzzzzt... ERROR: No input received.");
-            }
+            Part01();
+            Part02();
         }
     }
 }
