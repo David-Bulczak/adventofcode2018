3e901e0e862c3c6adeacabc4630aa7c35ae77d43
David Bulczak (Win) <david.bulczak@gmail.com>
2018-12-12 23:02:42 +0100

    Day 05 finished

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -9,14 +9,59 @@ namespace Day05
 {
     class Day05Tasks : DayTask
     {
-        public override void Part01()
+        public string ReactPolymer(string polymer)
         {
-            throw new NotImplementedException();
+            List<char> charList = new List<char>();
+            charList.AddRange(polymer);
+            for (int c = 0; c < charList.Count - 1;)
+            {
+                // compare successive elements
+                var current = charList[c];
+                var next = charList[c + 1];
+
+                bool diffPolarization = false;
+
+                if (65 <= current && current <= 90) // uppercase
+                    diffPolarization = diffPolarization || (current == (next - 32));
+                else // lowercase
+                    diffPolarization = diffPolarization || (current == (next + 32));
+
+                if (diffPolarization)
+                {
+                    charList.RemoveAt(c);
+                    charList.RemoveAt(c);
+
+                    // Since due to removing of chars the two surrounding may have different polarization
+                    // we need to "step back"
+                    if (c > 0)
+                        c--;
+                }
+                else
+                    ++c;
+            }
+            return string.Join("",charList.ToArray());
+        }
+        public override int Part01(string[] input = null)
+        {
+            //var resultString = string.Join(charList.ToString);
+            //System.Console.WriteLine("Remining poly: " + charList.ToString());
+            return ReactPolymer(input[0]).Length;
         }
 
-        public override void Part02()
+        public override int Part02(string[] input = null)
         {
-            throw new NotImplementedException();
+            int minLength = int.MaxValue;
+            for (char c = 'A'; c <= 'Z'; ++c)
+            {
+                string aux = input[0].Replace(c.ToString(), "");
+                aux = aux.Replace(((char)(c + 32)).ToString(), "");
+                aux = ReactPolymer(aux);
+
+                minLength = Math.Min(minLength, aux.Length);
+
+                //var replString = input.
+            }
+            return minLength;
         }
     }
 
@@ -27,7 +72,7 @@ namespace Day05
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Day05Tasks dayInst = new Day05Tasks();
-            dayInst.Exec(true, false);
+            dayInst.Exec(true, true);
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
