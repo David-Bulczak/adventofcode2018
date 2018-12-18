0333a54d5278bf06a8a6be1c0a93e440210bce1a
David Bulczak (Win) <david.bulczak@gmail.com>
2018-12-18 08:52:51 +0100

    Day 09

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -19,18 +19,42 @@ namespace Day09
 
     class MarbleGame
     {
-        
+        /**
+         * @brief Adds marbel into game and returns points
+         */
         public int AddMarble(int marbleNumber)
         {
+            if ((marbleNumber % 23) == 0)
+            {
+                int points = marbleNumber;
+                int removeId = CurrentId - 7;
+                if (removeId < 0)
+                    removeId = Marbles.Count - 1 - Math.Abs(removeId+1);
+                points += Marbles[removeId];
+                Marbles.RemoveAt(removeId);
+                CurrentId = removeId % Marbles.Count;
+                return points;
+            }
+            else
+            {
+                int oneStepId = (CurrentId + 1) % Marbles.Count;
+                int twoStepId = (CurrentId + 2) % Marbles.Count;
+
+                Marbles.Insert(oneStepId + 1, marbleNumber);
+                CurrentId = oneStepId + 1;
+                return 0;
+            }
             return 0;
         }
 
         public List<int> Marbles;
+        public int CurrentId;
 
         public MarbleGame()
         {
             Marbles = new List<int>();
             Marbles.Add(0);
+            CurrentId = 0;
         }
     }
 
@@ -38,7 +62,28 @@ namespace Day09
     {
         public override int Part01(string[] input = null)
         {
-            throw new NotImplementedException();
+            int numberOfPlayers = 479;
+            if (IsTesting)
+                numberOfPlayers = 9;
+            int numberOfMarbles = 71035 * 100;
+            if (IsTesting)
+                numberOfMarbles = 25;
+            var players = new List<Player>();
+            for (int p = 0; p < numberOfPlayers; ++p)
+                players.Add(new Player());
+
+            // actual algorithm
+            var game = new MarbleGame();
+            int currentPlayerId = 0; // actually in task +1
+            for (int s = 1; s <= numberOfMarbles; ++s)
+            {
+                int points = game.AddMarble(s);
+                players[currentPlayerId].Score += points;
+                ++currentPlayerId;
+                currentPlayerId = currentPlayerId % numberOfPlayers;
+            }
+            players.Sort((p0, p1) => p0.Score > p1.Score ? -1 : 1);
+            return players[0].Score;
         }
 
         public override int Part02(string[] input = null)
